929884bbb6e6389e25869aa79a8032c917ee53fb
Jojo <joseph.sharratt@gmail.com>
2016-05-31 19:06:03 +0100

    User can sign in with email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  def authenticate
+    if !signed_in?
+      redirect_to new_session_path
+    end
+  end
+
+  def signed_in?
+    session[:current_email].present?
+  end
+
   protect_from_forgery with: :exception
 end
--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 feature 'User creates todo' do
   scenario 'successfully' do
-    visit root_path
+    sign_in
 
     click_on 'Add a new todo'
     fill_in 'Title', with: 'Buy milkshake'
     click_on 'Submit'
-    
+
     expect(page).to have_css '.todos li', text: 'Buy milkshake'
   end
 end
